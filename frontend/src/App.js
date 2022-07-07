44a16599fdc523a8ed4298cb8b6af38a6a4db425
Ernst Legaspi <legaspiernstdanielle@gmail.com>
2022-07-07 09:55:45 +0800

    changes in enter_animal directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import React, { lazy, Suspense } from 'react'
+import React, { useRef, lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './css/global.css'
+import song from  "./css/song.mp3"
 
 const Home = lazy(() => import("./components/home/home"))
 const Form = lazy(() => import("./components/form/form"))
 
 const App = () => {
+	const aud = useRef()
+
+	window.onload = () => {
+		aud.current.play()
+	}
+	
 	return(
 		<div className="app-max-width">
+			<audio src={song} ref={aud} />
 			<BrowserRouter>
 				<Suspense fallback={<div />}>
 					<Routes>
